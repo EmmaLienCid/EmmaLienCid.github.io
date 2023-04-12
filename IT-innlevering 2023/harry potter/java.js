49ba05285b9a20cb744052cdd2a5390b214d252f
EmmaLienCid <130604491+EmmaLienCid@users.noreply.github.com>
2023-04-12 22:25:59 +0200

    En begynnelse på en nettside om Galtvort

---
+++ IT-innlevering 2023/harry potter/java.js
@@ -0,0 +1,22 @@
+
+const menuBTN = document.querySelector(".hamburger")
+let menuOpen = false;
+menuBTN.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBTN.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBTN.classList.remove('open');
+        menuOpen =false;
+    }
+});
+
+const toggleButton = document.getElementById('toggle-button');
+const naviList = document.getElementById('navi-list');
+
+toggleButton.addEventListener('click', () => {
+    naviList.classList.toggle('active');
+})
+
+
+
